3f1e1efaa05d4476427ae3efbddd3c824dce0824
Kirsten Keels <keelsk@gmail.com>
2020-02-29 23:33:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   
   def add_item(item, cost, quantity = 1)
     @total = @total + (quantity * cost)
-    @@items << item
+    @@items << self.item
   end
   
   def apply_discount
